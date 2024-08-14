b9a4a56f2446c656dc79af723ab2424a2c7f07be
Tirtharaj Das <tirtharaj.das@entecnia.com>
2024-08-14 12:00:40 +0530

    ADD

---
+++ TodoApp/TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoApp
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void todoBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(todo_input.Text))
+            {
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = todo_input.Text;
+                todo_stacks.Children.Add(textBlock);
+                todo_input.Clear();
+            }
+        }
+    }
+}
